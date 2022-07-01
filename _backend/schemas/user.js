303e80d5db1ba86a57cae342a1366f7934baa8e2
Mohamed Ibnahmad <ibnahmadmohamed8@gmail.com>
2022-07-01 17:02:30 +0100

    create user shchema

---
+++ _backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'User',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'username',
+            title: 'Username',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string',
+        }
+    ]
+}
