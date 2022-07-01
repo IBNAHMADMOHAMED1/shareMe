0b053cee0d41b9f547726e7a090e5c0030ba52d4
Mohamed Ibnahmad <ibnahmadmohamed8@gmail.com>
2022-07-01 17:12:17 +0100

    add schemas

---
+++ _backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'Comment',
+    title: 'Comment',
+    type: 'document',
+    fields: [
+        {
+            name: 'postBy',
+            title: 'PostBy',
+            type: 'string',
+        },
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string',
+        }
+    ]
+}
---
+++ _backend/schemas/save.js
@@ -0,0 +1,17 @@
+export default { 
+    name: 'Save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+        {
+            name: 'postBy',
+            title: 'PostBy',
+            type: 'string',
+        },
+        {
+            name: 'userId',
+            title: 'UserId',
+            type: 'string',
+        }
+    ]
+}
